39314ee120b76d3bf766543f2ee060ab248c4dc5
alicanozer60 <tokatli6o@hotmail.com>
2016-05-15 22:52:46 +0300

    configured environment to work with heroku

--- config/ozer.rb
+++ config/ozer.rb
@@ -2,5 +2,5 @@
 ENV['STORAGE_PATH'] = "uploads" if ENV['STORAGE_PATH'].nil?
 ENV['LOGIN_REQUIRE'] = 'true'
 ENV['TITLE'] = 'PHOTO GALLERY'
-ENV['LOGO'] =  'asdasdasd.png'
+ENV['LOGO'] =  'rails.png'
 
