d171aa0dfa7b43f5eb56fa7b4eea13599e3124f1
alicanozer60 <tokatli6o@hotmail.com>
2016-05-16 01:20:50 +0300

    resolved heroku errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     levels.delete_at(0)
 
     #links = "You are here: "
-    links = content_tag('a', "HOME", :href => root_path) if include_home
+    links = content_tag('a', "ANASAYFA", :href => root_path) if include_home
 
     nocrumb = ["collections", "albums", "photos", "tags", "new", "edit", "tags"]
 
